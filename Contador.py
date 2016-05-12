d6c58d7126669a10cacfb007be8d1177c086a652
CarolinaGonzalezS <carolay-96@live.com>
2016-05-12 08:08:23 -0500

    contador1.1
    aumente for

--- Contador.py
+++ Contador.py
@@ -1,4 +1,5 @@
 frase=str(input("ingrese una frase"))
-frase1=frase.split("__")
-print(frase1)for i in frase:
- 
+frase1=frase.split(",")
+print(frase1)
+for i in frase:
+    palabras=frase.lend(i.split())
