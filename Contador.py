6f704e03404cb8faca718e4766aadce154b0c875
CarolinaGonzalezS <carolay-96@live.com>
2016-05-12 08:13:04 -0500

    contador1.1.1
    final

--- Contador.py
+++ Contador.py
@@ -1,5 +1,4 @@
-frase=str(input("ingrese una frase"))
-frase1=frase.split(",")
+frase=("ingrese una frase"))
+frase1=frase.split(".")
 print(frase1)
-for i in frase:
-    palabras=frase.lend(i.split())
+#sdgdfghf
