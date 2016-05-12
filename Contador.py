9b35195c1bcf4f0c113eb795630ba82487decbff
CarolinaGonzalezS <carolay-96@live.com>
2016-05-12 08:33:35 -0500

    ContadorFinal
    siguiente paso aumentar lectura de texto

--- Contador.py
+++ Contador.py
@@ -1,5 +1,9 @@
-frase=str(input("ingrese una frase"))
-frase1=frase.split(".")
-print(frase1)
-#fdsdkjgkfddfdsrugduhyg
-#jfxghkjdfhlkfjg
+frase=str(input("ingrese una frase\n"))
+def contador(frase):
+    frase1=frase.splitlines()
+    print(frase1)
+    for i in range(len(frase1)):
+        palabras=len(frase1[i].split(" "))
+        print("palabra ",i,": ",palabras)
+
+contador(frase)
