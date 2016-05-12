23b62c6432fab0b68a8b83ed6d7e79dcf8bdbd43
CarolinaGonzalezS <carolay-96@live.com>
2016-05-12 09:07:39 -0500

    Contador_Final
    ya lee

--- Contador.py
+++ Contador.py
@@ -1,12 +1,5 @@
-frase=str(input("ingrese una frase\n"))
-
-def contador(frase):
-    frase1=frase.splitlines()
-    print(frase1)
-    for i in range(len(frase1)):
-        palabras=len(frase1[i].split(" "))
-        print("palabra ",i,": ",palabras)        
-
+#frase=str(input("ingrese una frase\n"))
+         
 
 def creartxt():
     archi=open('datos.txt', 'w')
@@ -21,11 +14,18 @@ def leertxt():
     linea=archi.readline()
     while linea!="":
         print (linea)
-        contador(linea)
+        contador(linea)        
         linea=archi.readline()
         contador(linea)
         archi.close()
 
-creartxt()
-grabartxt()
+def contador(frase):
+    frase1=frase.splitlines()
+    print(frase1)
+    for i in range(len(frase1)):
+        palabras=len(frase1[i].split(" "))
+        print("palabra ",i,": ",palabras)               
+
+#creartxt()
+#grabartxt()
 leertxt()
